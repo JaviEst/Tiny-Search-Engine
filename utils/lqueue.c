4cbac3fc9d78ff0782fe4b8cc690c7e10ed9b2f9
Garth Verdeflor <f00436n@hub6.thayer.dartmouth.edu>
2021-11-11 10:32:22 -0500

    Concurrent Queue Implemented the lqueue module. Need to verify with tests.

--- utils/lqueue.c
+++ utils/lqueue.c
@@ -17,89 +17,133 @@
 #include <pthread.h>
 #include "queue.h"
 
-
 typedef struct lqueue {
-    queue_t qp;
-    // mutex
+    queue_t *qp;
+    pthread_mutex_t lock;
 } lqueue_t;
 
-
 //---------------------------- lqopen ------------------------------
-// Description:   create an empty queue
+// Description:   create a locked empty queue
 // Inputs:        nothing
-// Outputs:       returns pointer to an empty queue
+// Outputs:       returns pointer to a locked empty queue
 //------------------------------------------------------------------
 lqueue_t* lqopen(void) {
-    
+    lqueue_t *empty_lq = malloc(sizeof(lqueue_t));
+
+    // Initialize mutex
+    pthread_mutex_init(&empty_lq->lock, NULL);
+    empty_lq->qp = qopen();
+    return empty_lq;
 }
 
 //---------------------------- lqclose -----------------------------
-// Description:   deallocate a queue, frees everything in it
-// Inputs:        queue pointer
-// Outputs:       deallocate queue, free memory
+// Description:   deallocates a locked queue
+// Inputs:        locked queue pointer
+// Outputs:       deallocate locked queue and frees memory
 //------------------------------------------------------------------
-void lqclose(lqueue_t *qp) {
-    
+void lqclose(lqueue_t *lqp) {
+    // Destroy the mutex
+    pthread_mutex_destroy(&lqp->lock);
+    qclose(lqp->qp);
 }
 
 //---------------------------- lqput -------------------------------
-// Description:   put element at the end of the queue
-// Inputs:        queue pointer and element to add to the queue
-// Outputs:       returns 0 is successful; nonzero otherwise
+// Description:   put element at end of locked queue
+// Inputs:        locked queue pointer and element to add to queue
+// Outputs:       returns 0 is successful, nonzero otherwise
 //------------------------------------------------------------------
-int32_t lqput(lqueue_t *qp, void *elementp) {
-    
+int32_t lqput(lqueue_t *lqp, void *elementp) {
+    // Wait for ownership of locked queue
+    pthread_mutex_lock(&lqp->lock);
+    int32_t success = qput(lqp->qp, elementp);
+
+    // Give up ownership of locked queue
+    pthread_mutex_unlock(&lqp->lock);
+    return success;
 }
 
 //---------------------------- lqget -------------------------------
-// Description:   get the first element from queue, 
+// Description:   get first element from locked queue, 
 //                removing it from the queue
-// Inputs:        queue pointer 
-// Outputs:       pointer to first element of the queue
-//                remove first element from the queue
+// Inputs:        locked queue pointer 
+// Outputs:       pointer to first element of locked queue
+//                (remove this element from locked queue)
 //------------------------------------------------------------------
-void* lqget(lqueue_t *qp) {    
-    
+void* lqget(lqueue_t *lqp) {    
+    // Wait for ownership of locked queue
+    pthread_mutex_lock(&lqp->lock);
+    void *elementp = qget(lqp->qp);
+
+    // Give up ownership of locked queue
+    pthread_mutex_unlock(&lqp->lock);
+    return elementp;
 }
 
-//---------------------------- qlapply -------------------------------
-// Description:   apply a function to every element of the queue
-// Inputs:        function to apply to every element in the queue
-//                queue pointer
-// Outputs:       apply a function to every element of the queue
+//---------------------------- lqapply ------------------------------
+// Description:   apply a function to every element of locked queue
+// Inputs:        function to apply to every element in locked queue,
+//                locked queue pointer
+// Outputs:       apply a function to every element of locked queue
 //-------------------------------------------------------------------
-void lqapply(lqueue_t *qp, void (*fn)(void* elementp)) {
-    
+void lqapply(lqueue_t *lqp, void (*fn)(void* elementp)) {
+    // Wait for ownership of locked queue
+    pthread_mutex_lock(&lqp->lock);
+    qapply(lqp->qp, fn);
+
+    // Give up ownership of locked queue
+    pthread_mutex_unlock(&lqp->lock);
 }
 
 //---------------------------- lqsearch -------------------------------
-// Description:   search a queue using a supplied boolean function
-// Inputs:        a search function, queue pointer, and key
+// Description:   search locked queue using supplied boolean function
+// Inputs:        a search function, locked queue pointer, and key
 // Outputs:       returns a pointer to an element, or NULL if not found
 //--------------------------------------------------------------------
-void* lqsearch(lqueue_t *qp, 
+void* lqsearch(lqueue_t *lqp, 
                 bool (*searchfn)(void* elementp,const void* keyp),
 				const void* skeyp) {
-    
+    // Wait for ownership of locked queue
+    pthread_mutex_lock(&lqp->lock);
+    void *elementp = qsearch(lqp->qp, searchfn, skeyp);
+
+    // Give up ownership of locked queue
+    pthread_mutex_unlock(&lqp->lock);
+    return elementp;
 }
 
 //---------------------------- lqremove ------------------------------
-// Description:   search a queue using a supplied boolean function 
-//                (as in qsearch), removes the element from the queue
-// Inputs:        a search function, queue pointer, and key
+// Description:   search locked queue using supplied boolean function 
+//                (as in qsearch), removes element from locked queue
+// Inputs:        a search function, locked queue pointer, and key
 // Outputs:       returns a pointer to the element, or NULL if not found
 //--------------------------------------------------------------------
-void* lqremove(lqueue_t *qp,
+void* lqremove(lqueue_t *lqp,
                 bool (*searchfn)(void* elementp,const void* keyp),
                 const void* skeyp) {
-    
+    // Wait for ownership of locked queue
+    pthread_mutex_lock(&lqp->lock);
+    void *elementp = qremove(lqp->qp, searchfn, skeyp);
+
+    // Give up ownership of locked queue
+    pthread_mutex_unlock(&lqp->lock);
+    return elementp;
 }
 
 //---------------------------- lqconcat ------------------------------
-// Description:   concatenates elements of q2 into q1
-// Inputs:        pointers to both queues
-// Outputs:       q2 is deallocated
+// Description:   concatenates elements of locked q2 int locked q1
+// Inputs:        pointers to both locked queues
+// Outputs:       locked q2 is deallocated
 //--------------------------------------------------------------------
-void lqconcat(lqueue_t *q1p, lqueue_t *q2p) {
-    
+void lqconcat(lqueue_t *lq1p, lqueue_t *lq2p) {
+    // Wait for ownership of locked queue
+    pthread_mutex_lock(&lq1p->lock);
+    pthread_mutex_lock(&lq2p->lock);
+    qconcat(lq1p->qp, lq2p->qp);
+
+    // Give up ownership of locked queue
+    pthread_mutex_unlock(&lq1p->lock);
+    pthread_mutex_unlock(&lq2p->lock);
+
+    // Destroy lock and free memory of second queue
+    lqclose(lq2p);
 }
